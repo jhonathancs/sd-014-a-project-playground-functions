dd85be617299f0d82da5fd93716aeac7a2cf81cf
jhonathancs <jhonathan_jcs@hotmail.com>
2021-07-01 13:02:08 -0500

    comit 11
    comit final

--- src/challenges2.js
+++ src/challenges2.js
@@ -39,7 +39,7 @@ function generatePhoneNumber(phone) {
   
 }
 
- console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
+//console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]))
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
